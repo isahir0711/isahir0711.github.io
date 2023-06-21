fb83d69c0554cf2e1255e85d3b6ec6f44e65bbb5
Isahir Zavala Miranda <isahirzavala@hotmail.com>
2023-06-21 16:46:50 -0500

    theme change buttonv2

--- scripts.js
+++ scripts.js
@@ -3,7 +3,6 @@ function setTheme(themeName) {
     document.documentElement.className = themeName;
 }
 
-
 function toggleTheme() {
     if (localStorage.getItem('theme') === 'theme-dark') {
         setTheme('theme-light');
@@ -13,23 +12,25 @@ function toggleTheme() {
 }
 
 (function () {
-    if (localStorage.getItem('theme') === 'theme-dark') {
+    const preferredTheme = localStorage.getItem('theme');
+    const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    if (preferredTheme === 'theme-dark' || (!preferredTheme && userPrefersDark)) {
         setTheme('theme-dark');
         document.getElementById('slider').checked = true;
     } else {
         setTheme('theme-light');
-      document.getElementById('slider').checked = false;
+        document.getElementById('slider').checked = false;
     }
 })(); 
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
     const newColorScheme = event.matches ? "dark" : "light";
-    if(newColorScheme === 'light'){
+    if (newColorScheme === 'light') {
         setTheme('theme-light');
         document.getElementById('slider').checked = false;
-    }
-    else if (newColorScheme === 'dark') {
+    } else if (newColorScheme === 'dark') {
         setTheme('theme-dark');
-      document.getElementById('slider').checked = true;
-    }  
+        document.getElementById('slider').checked = true;
+    }
 });
